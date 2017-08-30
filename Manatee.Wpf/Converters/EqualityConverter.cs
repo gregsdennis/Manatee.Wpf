0157be0b124eef4d696150e02154196939abb6d0
Greg Dennis <gregsdennis@yahoo.com>
2017-08-30 15:16:10 +1200

    added equality converter

---
+++ Manatee.Wpf/Converters/EqualityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Manatee.Wpf.Converters
+{
+	public class Equality : IMultiValueConverter
+	{
+		public static Equality AreEqual { get; }
+		public static Equality AreNotEqual { get; }
+
+		private readonly bool _invert;
+
+		static Equality()
+		{
+			AreEqual = new Equality(false);
+			AreNotEqual = new Equality(true);
+		}
+		private Equality(bool invert)
+		{
+			_invert = invert;
+		}
+
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			var distinct = values.Distinct().Count();
+			return _invert ? distinct != 1 : distinct == 1;
+		}
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
